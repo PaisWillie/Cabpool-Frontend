a5d5471b0f46acc034e9e416d10b9692e4f58374
Willie Pai <PaisWillie@gmail.com>
2023-04-08 16:45:03 -0400

    Linting

--- src/controllers/EncryptionController.tsx
+++ src/controllers/EncryptionController.tsx
@@ -1,29 +1,31 @@
-var encryption_key = "secret";
+var encryption_key = 'secret';
 
 const Encrypt = (input: string) => {
-  let delimiter = "-";
-    let encrypted = "";
+  let delimiter = '-';
+  let encrypted = '';
 
-    for(let i = 0; i < input.length; i++){
-        let ascii_code = input.charCodeAt(i);
-        let shifted_code = ascii_code + encryption_key.charCodeAt(i % encryption_key.length);
-        let encryptedChar = String.fromCharCode(shifted_code);
-        encrypted += encryptedChar + delimiter;
-    }
-    return encrypted;
+  for (let i = 0; i < input.length; i++) {
+    let ascii_code = input.charCodeAt(i);
+    let shifted_code =
+      ascii_code + encryption_key.charCodeAt(i % encryption_key.length);
+    let encryptedChar = String.fromCharCode(shifted_code);
+    encrypted += encryptedChar + delimiter;
+  }
+  return encrypted;
 };
 
 const Decrypt = (input: string) => {
-  let delimiter = "-";
-    let encryptedChars = input.split(delimiter);
-    let decrypted = "";
-    for(let i=0; i < encryptedChars.length; i++){
-        let ascii_code = encryptedChars[i].charCodeAt(0);
-        let shifted_code = ascii_code - encryption_key.charCodeAt(i % encryption_key.length);
-        let decryptedChar = String.fromCharCode(shifted_code);
-        decrypted += decryptedChar;
-    }
-    return decrypted;
+  let delimiter = '-';
+  let encryptedChars = input.split(delimiter);
+  let decrypted = '';
+  for (let i = 0; i < encryptedChars.length; i++) {
+    let ascii_code = encryptedChars[i].charCodeAt(0);
+    let shifted_code =
+      ascii_code - encryption_key.charCodeAt(i % encryption_key.length);
+    let decryptedChar = String.fromCharCode(shifted_code);
+    decrypted += decryptedChar;
+  }
+  return decrypted;
 };
 
 export {Encrypt, Decrypt};
