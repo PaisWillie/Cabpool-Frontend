0e0f39aa7b3e360a91c0d8e1215dfeab9691d159
Willie Pai <PaisWillie@gmail.com>
2023-04-07 20:38:44 -0400

    Moved action buttons to components

--- src/views/route-input/RouteInput.tsx
+++ src/views/route-input/RouteInput.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {View} from 'react-native';
-import {Button, FAB, Icon} from '@rneui/themed';
+import {Button} from '@rneui/themed';
 import RouteLocations from './components/RouteLocations';
 import SuggestedRides from './components/SuggestedRides';
 import Payment from './components/Payment';
+import FloatingActionButtons from './components/FloatingActionButtons';
 
 const RouteInputPage = () => {
   return (
@@ -15,18 +16,7 @@ const RouteInputPage = () => {
         height: '100%',
         justifyContent: 'space-between',
       }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}>
-        <FAB color="white">
-          <Icon name="menu" type="feather" color="black" />
-        </FAB>
-        <FAB color="white">
-          <Icon name="person-outline" type="Ionicons" color="white" />
-        </FAB>
-      </View>
+      <FloatingActionButtons />
       <View
         id="information-card"
         style={{
