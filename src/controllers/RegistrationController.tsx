8edf21da1cd8c688daf95232dcb73c567a4732ac
Daniel Akselrod <dakselrod@outlook.com>
2023-04-09 18:50:52 -0400

    Added API call

--- src/controllers/RegistrationController.tsx
+++ src/controllers/RegistrationController.tsx
@@ -1,10 +1,31 @@
-const RegisterAccount = (
+const RegisterAccount = async (
   firstName: string,
   lastName: string,
   email: string,
   password: string,
-): boolean => {
-  return false;
+) => {
+  try {
+    const response = await fetch('http://10.0.2.2:8082/user/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      }),
+    });
+    if (response.status >= 200 && response.status < 300) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
 };
 
 export default RegisterAccount;
