4c6a0853a1cba28c6619ae592d5f293e3e4548e1
Willie Pai <PaisWillie@gmail.com>
2023-04-10 16:02:43 -0400

    Removed blur

--- src/views/challenge/ChallengeStart.tsx
+++ src/views/challenge/ChallengeStart.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 
 import React from 'react';
-import {BlurView} from '@react-native-community/blur';
 import {View} from 'react-native';
 import GoogleMaps from '../../components/GoogleMaps';
 import {Divider, Text} from '@rneui/base';
@@ -16,20 +15,6 @@ const ChallengeStartPage = () => {
         justifyContent: 'flex-end',
       }}>
       <GoogleMaps />
-      <BlurView
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-          width: '100%',
-          height: '100%',
-        }}
-        blurType="dark"
-        blurAmount={32}
-        reducedTransparencyFallbackColor="white"
-      />
       <View
         id="information-card"
         style={{
