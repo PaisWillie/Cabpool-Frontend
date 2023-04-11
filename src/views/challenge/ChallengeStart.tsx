25de1833fb22d9bdfe4e668eb1c3f44e66eecdd8
Daniel Akselrod <dakselrod@outlook.com>
2023-04-11 13:59:28 -0400

    Added coin flip and page navigation

--- src/views/challenge/ChallengeStart.tsx
+++ src/views/challenge/ChallengeStart.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable react-native/no-inline-styles */
-
 import React from 'react';
-import {View} from 'react-native';
+import {View, TouchableOpacity} from 'react-native';
 import GoogleMaps from '../../components/GoogleMaps';
-import {Divider, Text} from '@rneui/base';
+import {Button, Divider, Text} from '@rneui/base';
 
-const ChallengeStartPage = () => {
+const ChallengeStartPage = ({navigation}: any) => {
   return (
     <View
       style={{
@@ -19,7 +17,6 @@ const ChallengeStartPage = () => {
         id="information-card"
         style={{
           backgroundColor: 'white',
-          flexDirection: 'column',
           elevation: 90,
           width: '100%',
           paddingHorizontal: 16,
@@ -27,13 +24,30 @@ const ChallengeStartPage = () => {
           borderTopLeftRadius: 20,
           borderTopRightRadius: 20,
         }}>
-        <Text style={{fontWeight: 'bold', fontSize: 16}}>Challenge</Text>
-        <Divider style={{marginVertical: 12}} />
-        <Text>Challenging other carpool riders to a coin flip!</Text>
-        <Text>
-          Winner receives the prize pool as a discount in their trip fare
-        </Text>
-        <Text>Cost to buy in: CAD $1.00</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginBottom: 12,
+          }}>
+          <Text style={{fontWeight: 'bold', fontSize: 16}}>Challenge</Text>
+          <Button
+            title="Start Game"
+            radius={10}
+            onPress={() => {
+              navigation.navigate('ChallengeResult');
+            }}
+          />
+        </View>
+        <Divider />
+        <View style={{marginTop: 12}}>
+          <Text>Challenging other carpool riders to a coin flip!</Text>
+          <Text>
+            Winner receives the prize pool as a discount in their trip fare
+          </Text>
+          <Text>Cost to buy in: CAD $1.00</Text>
+        </View>
       </View>
     </View>
   );
